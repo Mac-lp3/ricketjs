20bd53ba4d694a3a2a7f0c67eb25562786525989
Mac-lp3 <staticlp3@gmail.com>
2016-09-08 19:37:36 -0400

    added getBaseFileName function

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,32 @@
 
 module.exports = (function() {
 
+	const getBaseFileName = (stringFilePath, includeExtention) => {
+
+		let baseName = '';
+
+		// using forward or backward slashes?
+		if (stringFilePath.lastIndexOf('/') != -1) {
+
+			// get everything after the last /
+			baseName = stringFilePath.substring(stringFilePath.lastIndexOf('/') + 1);
+
+
+		} else if (stringFilePath.lastIndexOf('\\') != -1) {
+
+			// get everything after the last \
+			baseName = stringFilePath.substring(stringFilePath.lastIndexOf('\\') + 1);
+		}
+
+		// cut off extention?
+		if (!includeExtention) {
+			baseName = baseName.substring(0, baseName.lastIndexOf("."));
+		}
+
+		return baseName;
+
+	};
+
 	const isObject = (varToCheck) => {
 		return Object.prototype.toString.call(varToCheck) == '[object Object]';
 	};
