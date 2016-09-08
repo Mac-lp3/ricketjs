8bcfe20847f01de0be3a14680bcf7245c8e52529
Mac-lp3 <staticlp3@gmail.com>
2016-09-08 19:17:46 -0400

    added functionality for building middleware actions

--- js/taskBuilder.js
+++ js/taskBuilder.js
@@ -1,1 +1,73 @@
 'use strict';
+
+const util = require('./utils');
+const fs = require('fs');
+
+module.exports = (function() {
+
+	const globalBeforeFunctions = [];
+	const globalAfterFunctions = [];
+	const globalBeforeEachFunctions = [];
+	const globalAfterEachFunctions = [];
+	const orderedTaskList = [];
+
+	const globalErrorHandler = function (ex) {
+		throw ex;
+	};
+
+	const buildTask  = function(descriptor) {
+
+		const newTask = {};
+
+		if (util.isFunction(descriptor)) {
+
+			// this is a middleware function
+			newTask.name = descriptor.name;
+			newTask.command = descriptor;
+			newTask.befores = globalBeforeEachFunctions;
+			newTask.afters = globalAfterEachFunctions;
+			newTask.errorHandler = globalErrorHandler;
+
+		} else if (util.isObject(descriptor)) {
+
+			// this is an executable configuration
+
+		} else if (util.isString(descriptor)) {
+
+			// this is a path to an executable
+
+		}
+
+	};
+
+	const buildExecutableCommand = function (descriptor) {
+
+		// get name
+		// assemple action () if not function
+		// extract before/after
+		let task = {};
+
+	};
+
+	const clearAll = function () {
+
+		orderedTaskList.length = 0;
+		globalAfterFunctions.length = 0;
+		globalBeforeFunctions.length = 0;
+		globalAfterEachFunctions.length = 0;
+		globalBeforeEachFunctions.length = 0;
+
+	};
+
+	const getOrderedTaskList = function() {
+		return orderedTaskList;
+	};
+
+
+	return {
+		buildTask: buildTask,
+		clearAll: clearAll,
+		getTaskList: getOrderedTaskList
+	};
+
+})();
