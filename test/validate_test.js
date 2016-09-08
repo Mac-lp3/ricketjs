7612379bdfc68b4b2f66312c94de261ce2927df0
Mac-lp3 <staticlp3@gmail.com>
2016-09-07 21:36:29 -0400

    added test cases for descriptor

--- test/validate_test.js
+++ test/validate_test.js
@@ -10,12 +10,17 @@ describe('validate', function() {
         it('Should interpret each type', function() {
         	
         	let e = undefined;
-            let isValid = false;
+            let isValid = true;
         	
         	try {
 
             	isValid = validate.filePath('test\\path');
                 assert(!isValid);
+                isValid = true;
+
+                isValid = validate.filePath('');
+                assert(!isValid);
+
                 isValid = validate.filePath('test\\path\\name.txt');
                 assert(isValid);
 
@@ -36,10 +41,19 @@ describe('validate', function() {
         it('Validate an object descriptor of an executable', function() {
             
             let e = undefined;
+            let isValid = true;
             
             try {
 
-                validate.descriptor({name: 'test name'});
+                isValid = validate.descriptor({path: 'test\\path'});
+                assert(!isValid);
+                isValid = true;
+
+                isValid = validate.descriptor({what: 'test\\path'});
+                assert(!isValid);
+
+                isValid = validate.descriptor({path: 'test\\path\\.txt'});
+                assert(isValid);
 
             } catch (ex) {
 
