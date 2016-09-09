b2dd4f51761bec4a0269e71cc4373c8096cabaef
Mac-lp3 <staticlp3@gmail.com>
2016-09-09 18:43:39 -0400

    added reset function.

--- ricket.js
+++ ricket.js
@@ -5,6 +5,8 @@ const taskBuilder = require('./js/taskBuilder');
 
 module.exports = (function() {
 
+	let currentTaskIndex = 0;
+
 	/*
 	 * single method to add middleware or a script
 	 */
@@ -29,17 +31,42 @@ module.exports = (function() {
 
 	const configure = (obj) => {
 
-
 		return interfaze;
 	};
 
-	const execute = () => {
+	/*
+	 * Core method that checks for next in the list, runs it, and manages
+	 * argument transmission.
+	 */
+	const execute = (valsFromPrevious) => {
 
+		// if current index == 0, just jump to befores.
+		// if next index > list lenth, pass in end function.
+
+		// get previous task.
+		// does it have any afters?
+		// run them, with valsFromPrevious, updating valsFromPrevious
+		// get current task
+		// any befores? 
+		// run them, with valsFromPrevious, updating valsFromPrevious
+		// update current task index.
+		// run the command with valsFromPrevious.
+
+	};
+
+	const endExecution = (valsFromPrevious) => {
+		return valsFromPrevious;
+	},
+
+	const resetState = () => {
+		taskBuilder.clear();
+		currentTaskIndex = 0;
 	};
 
 	const interfaze = {
 		add: add,
 		options: configure,
+		clear: resetState,
 		run: execute
 	};
 
